4ed116dbccb8b9fc26b99b374bd25e03b67e89fa
attah ifeanyi <attahsixtus6@gmail.com>
2022-09-24 01:59:11 +0100

    deleted lectures files

--- script.js
+++ script.js
@@ -130,12 +130,4 @@ btnLogin.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
