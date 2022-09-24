0b565d74da6553b742708253386a4dfadf6cbb3d
attah ifeanyi <attahsixtus6@gmail.com>
2022-09-24 02:27:20 +0100

    functions

--- script.js
+++ script.js
@@ -58,6 +58,7 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 //////////////////////////////////////////////////////////////////////
+//// functions ////
 const displayMovements = function (movements) {
   containerMovements.innerHTML = '';
 
